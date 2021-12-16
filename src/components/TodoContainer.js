30f6a5171fa54e7e6feb6e8f46ec98a0d8419c15
ThaoTran <thaott1901@gmail.com>
2021-12-16 02:49:41 -0500

    Part 2: Using the setState updater

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import TodosList from "./TodosList";
+import Header from "./Header";
 
 class TodoContainer extends React.Component {
     state = {
@@ -22,10 +23,28 @@ class TodoContainer extends React.Component {
         ]
     };
 
+    handleChange = (id) => {
+        this.setState(prevState => ({
+            todos: prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    }
+                }
+                return todo
+            }),
+        }));
+    };
+
     render() {
         return (
             <div>
-                <TodosList todos={this.state.todos} />
+                <Header />
+                <TodosList 
+                    todos={this.state.todos} 
+                    handleChangeProps={this.handleChange} 
+                />
             </div>
         );
     }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-// class TodoItem extends React.Component {
-//     render() {
-//         return <li>{this.props.todo.title}</li>
-//     }
-// }
-
-function TodoItem(props) {
-    return <li>{props.todo.title}</li>
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <li>
+                <input 
+                    type="checkbox" 
+                    checked={this.props.todo.completed} 
+                    onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+                />
+                {this.props.todo.title}
+            </li>
+        )
+    }
 }
+
+// function TodoItem(props) {
+//     return <li>{props.todo.title}</li>
+// }
 
 export default TodoItem
